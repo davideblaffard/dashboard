314a5c71b74bdfe52f48db6d2cd6d6c248f04cf0
davideblaffard <infodavide98@gmail.com>
2025-08-13 15:00:48 +0200

    fix: mapped logout

--- src/main/java/org/pizzeria/dashboard/controller/AuthController.java
+++ src/main/java/org/pizzeria/dashboard/controller/AuthController.java
@@ -3,6 +3,10 @@ package org.pizzeria.dashboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Controller
 public class AuthController {
 
@@ -11,4 +15,19 @@ public class AuthController {
         return "login";
     }
     
+     @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        // Invalida la sessione
+        request.getSession().invalidate();
+
+        // Cancella eventuali cookie di autenticazione
+        for (Cookie cookie : request.getCookies()) {
+            cookie.setMaxAge(0);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+        }
+
+        // Reindirizza alla login page
+        return "redirect:/login?logout";
+    }
 }
