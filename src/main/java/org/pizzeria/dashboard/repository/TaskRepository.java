75480200ffcb175958f3eda0a202feab60876757
davideblaffard <infodavide98@gmail.com>
2025-08-14 02:27:42 +0200

    create: added task repo and controller

--- src/main/java/org/pizzeria/dashboard/repository/TaskRepository.java
+++ src/main/java/org/pizzeria/dashboard/repository/TaskRepository.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 import org.pizzeria.dashboard.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+
     List<Task> findByDueDateBetween(LocalDate start, LocalDate end);
+    List<Task> findByDueDate(LocalDate dueDate);
+    long countByDueDate(LocalDate dueDate);
 }
